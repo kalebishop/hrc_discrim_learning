bb8881ba40431f915bf016e3847eba2042c9c196
Kayleigh Bishop <kayleigh.bishop@yale.edu>
2019-07-24 19:00:30 +0000

    fixed crash issue by changing msg to srv setup for training

---
+++ src/auto_train.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+from hrc_discrim_learning.srv import *
+import rospy
+
+class ScriptedTrainer:
+    def __init__(self):
+        self.training_script = [
+            (4, "left block"),
+            (1, "right block"),
+            (2, "close block"),
+            (5, "far block")
+        ]
+
+        self.NEW_ENV    = 2
+        self.CONTINUE   = 1
+        self.STOP_TRAIN = 0
+
+        self.run        = True
+
+    def gen_input(self, req):
+        rospy.loginfo("Got request")
+        if not self.training_script:
+            self.run = False
+            return TrainInputResponse(self.STOP_TRAIN, 0, "")
+        else:
+            id, utt = self.training_script.pop()
+            return TrainInputResponse(self.CONTINUE, id, utt)
+
+    def run_train_server(self):
+        rospy.init_node('scripted_training')
+        s = rospy.Service('train_input_provider', TrainInput, self.gen_input)
+        while self.run:
+            rospy.sleep(10)
+
+if __name__ == '__main__':
+    t = ScriptedTrainer()
+    t.run_train_server()
+
+# TRAINING_DATA = [
+#     (4, "left block"),
+#     (1, "right block"),
+#     (2, "close block"),
+#     (5, "far block"),
+#     (0, "TRAIN STOP")
+# ]
+#
+# def main():
+#     rospy.init_node('auto_train')
+#     pub = rospy.Publisher('/hrc_discrim_learning/training_input', TrainInput, queue_size=1)
+#
+#     # rospy.sleep(10)
+#
+#     i = 0
+#     while (not rospy.is_shutdown()) and (i < len(TRAINING_DATA)):
+#         rospy.loginfo('Looping through training data')
+#         id, utt = TRAINING_DATA[i]
+#         pub.publish(TrainInput(id, String(utt)))
+#         rospy.sleep(1)
+#         i += 1
+#
+# if __name__ == '__main__':
+#     main()
--- src/train_spatial.py
+++ src/train_spatial.py
@@ -2,15 +2,18 @@
 from base import Object, AdaptiveContext
 from simple_listener import SimpleListener
 from spatial_learning import ImageLocationLearner, ObjectLocationLearner
-from hrc_discrim_learning.msg import TrainInput
+from hrc_discrim_learning.srv import TrainInput
 import env_perception
 import rospy
+import threading
 
 class TrainSpatialData:
     def __init__(self):
         # control msg mappings
-        self.cmd_new_env = "TRAIN NEW ENV"
-        self.cmd_stop_train = "TRAIN STOP"
+        self.NEW_ENV    = 2
+        self.CONTINUE   = 1
+        self.STOP_TRAIN = 0
+        self.ERROR      = -1
 
         self.listener = SimpleListener()
         self.img_loc_learner = ImageLocationLearner()
@@ -20,11 +23,38 @@ class TrainSpatialData:
 
         self.corpus_dict = {}
         rospy.init_node('train_spatial', anonymous=False)
-        rospy.Subscriber('/hrc_discrim_learning/training_input', TrainInput, self.receive_train_input)
-        # rospy.Subscriber('/hrc_discrim_learning/training_control', std_msgs/String, self.control)
         self.context = self.init_new_environment()
 
-        rospy.spin()
+        self.run_training()
+
+    def run_training(self):
+        while not rospy.is_shutdown():
+            input = self.receive_train_input()
+            if input == self.ERROR:
+                break
+            elif input == self.STOP_TRAIN:
+                self.end_train_input()
+                break
+            elif input == self.NEW_ENV:
+                self.context = self.init_new_environment()
+                self.corpus_dict[self.context] = []
+
+            # successfully got new input tuple to train
+            obj_id, obj_utt = input
+
+            # match id to obj in env
+            print("object id = ", obj_id)
+            print("context ids = ", [x.features['id'] for x in self.context.env])
+
+            ref_obj = None
+            for obj in self.context.env:
+                if obj.get_feature_class_value('id') == obj_id:
+                    ref_obj = obj
+
+            processed_utt = self.listener.get_named_features_as_tuples(obj_utt)
+
+            self.corpus_dict[self.context].append((ref_obj, processed_utt))
+            print(self.corpus_dict)
 
     def init_new_environment(self):
         # TODO: receive message from system abt whether to init a new env
@@ -33,37 +63,28 @@ class TrainSpatialData:
         self.corpus_dict[context] = []
         return context
 
-    def receive_train_input(self, input):
-        # rospy.loginfo("Training callback...", input.utterance.data)
-        if rospy.get_param('/hrc_discrim_learning/train_new_env') == 'true':
-            self.context = self.init_new_environment()
-            rospy.set_param('/hrc_discrim_learning/train_new_env', 'false')
-
-        obj_id = input.id
-        obj_utt = str(input.utterance.data)
-
-        if obj_utt == self.cmd_stop_train:
-            self.end_train_input()
-            return
-
-        print("object id = ", obj_id)
-        print("context ids = ", [x.features['id'] for x in self.context.env])
-
-        ref_obj = None
-        for obj in self.context.env:
-            if obj.get_feature_class_value('id') == obj_id:
-                ref_obj = obj
-
-        processed_utt = self.listener.get_named_features_as_tuples(obj_utt)
-
-        self.corpus_dict[self.context].append((ref_obj, processed_utt))
-        print(self.corpus_dict)
-
     def end_train_input(self):
+        print("Finishing train data collection")
+        # rospy.loginfo("Finished collection of training data")
         X, Y = self.img_loc_learner.preprocess(self.corpus_dict)
         self.img_loc_learner.train(X, Y)
         self.img_loc_learner.print_function()
         # self.obj_loc_learner.train(self.corpus_dict)
+
+    def receive_train_input(self):
+        rospy.wait_for_service('train_input_provider')
+        try:
+            input_provider = rospy.ServiceProxy('train_input_provider', TrainInput)
+            resp = input_provider(1)
+
+            if resp.control == self.STOP_TRAIN:
+                return self.STOP_TRAIN
+            else:
+                return resp.id, resp.utterance
+
+        except rospy.ServiceException:
+            print("Service call failed: %s")
+            return self.ERROR
 
 if __name__ == '__main__':
     trainer = TrainSpatialData()
